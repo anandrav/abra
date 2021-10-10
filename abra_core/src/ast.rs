6be6505043d56e28774e706227be27874e3d7c39
Anand Dukkipati <aRdukkipati@gmail.com>
2021-10-09 23:21:55 -0500

    parsing let expressions

--- abra_core/src/ast.rs
+++ abra_core/src/ast.rs
@@ -1,7 +1,19 @@
 #[derive(Debug)]
 pub enum Expr {
+    Var(String),
     Int(i32),
     BinOp(Box<Expr>, BinOpcode, Box<Expr>),
+    Let(Box<Pat>, Option<Type>, Box<Expr>, Box<Expr>)
+}
+
+#[derive(Debug)]
+pub enum Pat {
+    Var(String)
+}
+
+#[derive(Debug)]
+pub enum Type {
+    Int
 }
 
 #[derive(Debug)]
--- abra_core/src/parser.rs
+++ abra_core/src/parser.rs
@@ -4,8 +4,12 @@ pub fn do_stuff() {
     // let expr = abra_grammar::ExprParser::new()
     //     .parse("22 * 44 + 66; 85")
     //     .unwrap();
+
+    // let to_parse = "(2 + 3) * 4";
+    let to_parse = "let x : int = 2 in (x + x)";
+    println!("{}", to_parse);
     let expr = abra_grammar::ExprParser::new()
-        .parse("(2 + 3) * 4")
+        .parse(to_parse)
         .unwrap();
     println!("{}", format!("{:#?}", expr));
 }
