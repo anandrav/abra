1157a53344285fd98454e1aefbe3a6cdc75c8099
Anand Dukkipati <aRdukkipati@gmail.com>
2022-08-22 13:17:09 -0500

    added a grammar

--- src/edit_tree.rs
+++ src/edit_tree.rs
@@ -1,10 +1,13 @@
 use operators::BinOpcode;
+use pest_derive::Parser;
 use regex::Regex;
 use std::collections::VecDeque;
 use std::rc::Rc;
 use types::Type;
 
-use crate::parse_tree as pt;
+#[derive(Parser)]
+#[grammar = "grammar.pest"] // relative to src
+struct MyParser;
 
 pub type Identifier = String;
 pub type FuncArg = (Identifier, Option<Rc<Type>>);
@@ -34,7 +37,7 @@ impl ToString for Token {
 #[derive(Debug, PartialEq)]
 pub enum TokenTree {
     Leaf(Token),
-    Children(Vec<TokenTree>),
+    Children(Vec<Rc<TokenTree>>),
 }
 
 #[derive(Debug, PartialEq)]
@@ -76,51 +79,9 @@ pub enum ExprKind {
     Func(FuncArg, Vec<FuncArg>, Option<Rc<Type>>, Rc<Expr>),
     If(Rc<Expr>, Rc<Expr>, Rc<Expr>),
     Block(VecDeque<Rc<Stmt>>),
-    OpSeq(VecDeque<Op>),
+    BinOp(Rc<Expr>, Operator, Rc<Expr>),
     FuncAp(Rc<Expr>, Rc<Expr>, VecDeque<Rc<Expr>>),
 }
-
-impl Expr {
-    pub fn from(parsed: Rc<pt::Expr>) -> Rc<Self> {
-        let exprkind = match &*parsed.exprkind {
-            pt::ExprKind::EmptyHole => ExprKind::EmptyHole,
-            pt::ExprKind::InvalidText(s) => ExprKind::InvalidText(s.to_string()),
-            pt::ExprKind::Var(id) => ExprKind::Var(id.to_string()),
-            pt::ExprKind::Str(s) => ExprKind::Str(s.to_string()),
-            pt::ExprKind::Func(arg1, args, ty, body) => ExprKind::Func(
-                arg1.clone(),
-                args.clone(),
-                ty.clone(),
-                Expr::from(body.clone()),
-            ),
-            pt::ExprKind::FuncAp(f, arg1, args) => ExprKind::FuncAp(
-                Expr::from(f.clone()),
-                Expr::from(arg1.clone()),
-                args.iter().map(|x| Expr::from(x.clone())).collect(),
-            ),
-            _ => {
-                unimplemented!()
-            }
-        };
-        Rc::new(Expr { exprkind })
-    }
-
-    // TODO anandduk: maybe re-parse the text of the smallest expression that was modfied. If it parses to something, that's the new expression.
-    // Keep it stupid simple
-    pub fn perform_action(&self, action: &Action) -> Rc<Self> {
-        match action {
-            Action::InsertChar { c, loc } => match self {
-                Expr {
-                    exprkind: ExprKind::EmptyHole,
-                } => panic!(),
-                _ => unimplemented!(),
-            },
-            _ => unimplemented!(),
-        }
-    }
-}
-
-pub type Rule = (Rc<Pat>, Rc<Expr>);
 
 #[derive(Debug, PartialEq)]
 pub struct Pat {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate lalrpop_util;
 extern crate abra;
+extern crate pest;
+extern crate pest_derive;
 extern crate regex;
 
 mod edit_tree;
