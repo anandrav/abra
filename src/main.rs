ab5498e9f529ad4ff9c6931834f94dafd623029e
Anand Dukkipati <aRdukkipati@gmail.com>
2022-08-23 22:52:30 -0500

    removed some ui elements

--- src/main.rs
+++ src/main.rs
@@ -43,15 +43,6 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Abra Editor");
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
             ui.text_edit_multiline(&mut self.name);
         });
     }
