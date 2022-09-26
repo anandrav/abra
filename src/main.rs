dbb30e63fbf62609c357c8b4c65b8e456c052c90
Anand Dukkipati <aRdukkipati@gmail.com>
2022-09-26 00:29:02 -0500

    cursor movement

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,21 @@ mod operators;
 mod parse_tree;
 mod token_tree;
 mod types;
-use eframe::egui;
+use std::rc::Rc;
+
+use eframe::{
+    egui::{
+        self,
+        style::{Margin, Spacing},
+        Color32, Label, RichText,
+    },
+    epaint::Vec2,
+};
+
+use token_tree::*;
 
 fn main() {
-    let tt = token_tree::TokenTree::from("2 + 3");
+    let tt = TokenTree::from("2 + 3;");
     // dbg!(tt);
     println!("{}", tt.to_string());
 
@@ -27,22 +38,129 @@ fn main() {
 
 struct MyApp {
     text: String,
+    program: Rc<TokenTree>,
+    cursor: Cursor,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            text: "2 + 3;".to_owned(),
+            text: "2 +3 +  83 + 9;".to_owned(),
+            program: TokenTree::from("2 +3 +  83 + 9;"),
+            cursor: Cursor::Point(Location {
+                token: 0,
+                tposition: 0,
+            }),
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        self.text = token_tree::fix(&mut self.text);
+        if ctx.input().key_pressed(egui::Key::ArrowLeft) {
+            println!("key left pressed");
+            handle_action(Action::MoveLeft, &mut self.program, &mut self.cursor);
+        }
+        if ctx.input().key_pressed(egui::Key::ArrowRight) {
+            println!("key right pressed");
+            handle_action(Action::MoveRight, &mut self.program, &mut self.cursor);
+        }
+        self.text = fix(&mut self.text);
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Abra Editor");
-            ui.text_edit_multiline(&mut self.text);
+            // ui.text_edit_multiline(&mut self.text);
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    let mut tokens: Vec<Token> = vec![];
+                    fn collect_leaves(tt: &Rc<TokenTree>, leaves: &mut Vec<Token>) {
+                        match &tt.contents {
+                            Contents::Token(t) => leaves.push(t.clone()),
+                            Contents::Children(children) => {
+                                for c in children.iter() {
+                                    collect_leaves(c, leaves)
+                                }
+                            }
+                        }
+                    }
+
+                    collect_leaves(&self.program, &mut tokens);
+
+                    match (&self.cursor) {
+                        Cursor::Point(Location { token, tposition }) => {
+                            let index = token;
+                            for i in 0..*index {
+                                ui.spacing_mut().item_spacing = Vec2 { x: 0.0, y: 0.0 };
+                                ui.label(
+                                    RichText::new(tokens[i].to_string())
+                                        .monospace()
+                                        .color(Color32::WHITE)
+                                        .background_color(Color32::DARK_GRAY),
+                                );
+                            }
+                            if tokens[*index].num_points() == 2 {
+                                if *tposition != 0 {
+                                    ui.spacing_mut().item_spacing = Vec2 { x: 0.0, y: 0.0 };
+                                    ui.label(
+                                        RichText::new(tokens[*index].to_string())
+                                            .monospace()
+                                            .color(Color32::WHITE)
+                                            .background_color(Color32::DARK_GRAY),
+                                    );
+                                }
+                                ui.spacing_mut().item_spacing = Vec2 { x: 0.0, y: 0.0 };
+                                ui.label(
+                                    RichText::new("|")
+                                        .color(Color32::RED)
+                                        .background_color(Color32::DARK_GRAY),
+                                );
+                                if *tposition == 0 {
+                                    ui.spacing_mut().item_spacing = Vec2 { x: 0.0, y: 0.0 };
+                                    ui.label(
+                                        RichText::new(tokens[*index].to_string())
+                                            .monospace()
+                                            .color(Color32::WHITE)
+                                            .background_color(Color32::DARK_GRAY),
+                                    );
+                                }
+                            } else {
+                                let mut first = tokens[*index].to_string();
+                                let second = first.split_off(*tposition);
+                                println!("{}, {}", first, second);
+                                ui.spacing_mut().item_spacing = Vec2 { x: 0.0, y: 0.0 };
+                                ui.label(
+                                    RichText::new(first.to_string())
+                                        .monospace()
+                                        .color(Color32::WHITE)
+                                        .background_color(Color32::DARK_GRAY),
+                                );
+                                ui.spacing_mut().item_spacing = Vec2 { x: 0.0, y: 0.0 };
+                                ui.label(
+                                    RichText::new("|")
+                                        .color(Color32::RED)
+                                        .background_color(Color32::DARK_GRAY),
+                                );
+                                ui.spacing_mut().item_spacing = Vec2 { x: 0.0, y: 0.0 };
+                                ui.label(
+                                    RichText::new(second.to_string())
+                                        .monospace()
+                                        .color(Color32::WHITE)
+                                        .background_color(Color32::DARK_GRAY),
+                                );
+                            }
+                            for i in *index + 1..tokens.len() {
+                                ui.spacing_mut().item_spacing = Vec2 { x: 0.0, y: 0.0 };
+                                ui.label(
+                                    RichText::new(tokens[i].to_string())
+                                        .monospace()
+                                        .color(Color32::WHITE)
+                                        .background_color(Color32::DARK_GRAY),
+                                );
+                            }
+                        }
+                        Cursor::Selection { begin, end } => unimplemented!(),
+                    }
+                });
+            });
         });
     }
 }
--- src/token_tree.rs
+++ src/token_tree.rs
@@ -12,39 +12,185 @@ use types::Type;
 #[grammar = "grammar.pest"]
 struct MyParser;
 
-pub type Identifier = String;
-pub type FuncArg = (Identifier, Option<Rc<Type>>);
+// pub type Identifier = String;
+// pub type FuncArg = (Identifier, Option<Rc<Type>>);
+
+pub fn handle_action(action: Action, tree: &mut Rc<TokenTree>, cursor: &mut Cursor) {
+    match action {
+        Action::MoveLeft => match cursor {
+            Cursor::Point(Location { token, tposition }) => {
+                let t = tree.token_at_index(*token).unwrap();
+                if *tposition == 0 {
+                    *cursor = Cursor::Point(Location {
+                        token: *token - 1,
+                        tposition: t.num_points() - 1,
+                    })
+                } else {
+                    *cursor = Cursor::Point(Location {
+                        token: *token,
+                        tposition: *tposition - 1,
+                    })
+                }
+            }
+            _ => unimplemented!(),
+        },
+        Action::MoveRight => match cursor {
+            Cursor::Point(Location { token, tposition }) => {
+                let t = tree.token_at_index(*token).unwrap();
+                if *tposition == t.num_points() - 1 {
+                    *cursor = Cursor::Point(Location {
+                        token: *token + 1,
+                        tposition: 0,
+                    })
+                } else {
+                    *cursor = Cursor::Point(Location {
+                        token: *token,
+                        tposition: *tposition + 1,
+                    })
+                }
+            }
+            _ => unimplemented!(),
+        },
+        _ => (),
+    }
+}
+
+fn move_cursor(tree: &Rc<TokenTree>, cursor: &mut Cursor) {}
+
+#[derive(Debug, PartialEq)]
+pub enum Cursor {
+    Point(Location),
+    Selection { begin: Location, end: Location },
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Location {
+    pub token: usize,
+    pub tposition: usize,
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Action {
+    MoveLeft,
+    MoveRight,
     InsertChar { c: char, loc: usize },
     Backspace { loc: usize },
 }
 
-// #[derive(Debug, PartialEq)]
-// pub enum Token {
-//     Space,
-//     Tab,
-//     Newline,
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    // Whitespace
+    Space,
+    Tab,
+    Newline,
 
-//     OpenDelim(Delimiter),
-//     CloseDelim(Delimiter),
+    Placeholder,
 
-//     Identifier(String),
+    // Delimiter
+    OpenParen,
+    CloseParen,
+    OpenBrace,
+    CloseBrace,
+    OpenBracket,
+    CloseBracket,
 
-//     OpEq,
-//     OpAdd,
-//     OpSub,
-//     OpMult,
+    Identifier(String),
+    IntLit(String),
+    StrLit(String),
+    BoolLit(String),
 
-//     Semicolon,
-// }
+    // Operators
+    OpAssign,
+    OpEq,
+    OpAdd,
+    OpSub,
+    OpMult,
 
-// impl ToString for Token {
-//     fn to_string(&self) -> String {
-//         unimplemented!()
-//     }f
-// }
+    Semicolon,
+
+    // Keywords
+    Let,
+    If,
+    Else,
+}
+
+impl Token {
+    fn from(rule: &Rule, s: &str) -> Self {
+        match rule {
+            Rule::placeholder => Self::Placeholder,
+            Rule::block_start => Self::OpenBrace,
+            Rule::block_end => Self::CloseBrace,
+            Rule::identifier => Self::Identifier(s.to_string()),
+            Rule::literal_number => Self::IntLit(s.to_string()),
+            Rule::literal_string => Self::StrLit(s.to_string()),
+            Rule::literal_bool => Self::BoolLit(s.to_string()),
+            Rule::op_assign => Self::OpAssign,
+            Rule::op_eq => Self::OpEq,
+            Rule::op_addition => Self::OpAdd,
+            Rule::op_subtraction => Self::OpSub,
+            Rule::op_multiplication => Self::OpMult,
+            Rule::semicolon => Self::Semicolon,
+            Rule::let_keyword => Self::Let,
+            Rule::if_keyword => Self::If,
+            Rule::else_keyword => Self::Else,
+            Rule::WHITESPACE => match s {
+                " " => Self::Space,
+                "\t" => Self::Tab,
+                "\n" => Self::Newline,
+                _ => panic!("String {} not a WHITESPACE token!", s),
+            },
+            _ => panic!("Rule {:#?} not a primitive token!", rule),
+        }
+    }
+
+    fn to_str(&self) -> String {
+        use self::Token::*;
+        let s = match self {
+            Space => " ",
+            Tab => "\t",
+            Newline => "\n",
+            Placeholder => "_placeholder_",
+            OpenParen => "(",
+            CloseParen => ")",
+            OpenBrace => "{",
+            CloseBrace => "}",
+            OpenBracket => "[",
+            CloseBracket => "]",
+            Identifier(s) => s,
+            IntLit(s) => s,
+            StrLit(s) => s,
+            BoolLit(s) => s,
+            OpAssign => "=",
+            OpEq => "==",
+            OpAdd => "+",
+            OpSub => "-",
+            OpMult => "*",
+            Semicolon => ";",
+            Let => "let",
+            If => "if",
+            Else => "else",
+            _ => panic!(),
+        };
+        s.to_owned()
+    }
+
+    pub fn num_points(&self) -> usize {
+        use self::Token::*;
+        match self {
+            Space | Tab | Placeholder | OpenParen | CloseParen | OpenBrace | CloseBrace
+            | OpenBracket | CloseBracket | OpAssign | OpAdd | OpSub | OpMult | Semicolon | Let
+            | If | Else | OpEq => 2,
+            Identifier(s) | IntLit(s) | StrLit(s) | BoolLit(s) => s.len() + 1,
+            Newline => 0,
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_str())
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Kind {
@@ -77,42 +223,18 @@ impl Kind {
 
 #[derive(Debug, PartialEq)]
 pub struct TokenTree {
-    kind: Rule,
-    contents: TokenContents,
+    pub form: Rule,
+    pub contents: Contents,
 }
 
 #[derive(Debug, PartialEq)]
-pub enum TokenContents {
-    Token(String),
+pub enum Contents {
+    Token(Token),
     Children(Vec<Rc<TokenTree>>),
 }
 
-// pub fn fix(s: &str) -> String {
-//     println!("fix: {}", s);
-//     let _ = MyParser::parse(Rule::program, &s).unwrap_or_else(|e| match e {
-//         _ => {
-//             if let ErrorVariant::ParsingError {
-//                 positives,
-//                 negatives,
-//             } = e.variant
-//             {
-//                 if positives.contains(&Rule::placeholder) {
-//                     let mut s = String::from(s);
-//                     if let Pos(p) = e.location {
-//                         s.insert_str(p, "_placeholder_");
-//                         return fix(&s);
-//                     }
-//                 }
-//             }
-//             println!("{:#?}", e);
-//             panic!()
-//         }
-//     });
-//     s.to_string()
-// }
-
 pub fn fix(s: &str) -> String {
-    println!("fix: {}", s);
+    // println!("fix: {}", s);
     if let Err(e) = MyParser::parse(Rule::program, &s) {
         if let ErrorVariant::ParsingError {
             positives,
@@ -144,12 +266,12 @@ impl TokenTree {
         let as_str = pair.as_str();
         let children: Vec<_> = pair.into_inner().map(|x| TokenTree::from_pair(x)).collect();
         let contents = if !children.is_empty() {
-            TokenContents::Children(children)
+            Contents::Children(children)
         } else {
-            TokenContents::Token(as_str.to_string())
+            Contents::Token(Token::from(&rule, &as_str))
         };
         Rc::new(TokenTree {
-            kind: rule,
+            form: rule,
             contents,
         })
     }
@@ -160,24 +282,31 @@ impl TokenTree {
         Self::from_pair(pair)
     }
 
-    // pub fn from(s: &str) {
-    //     let pairs = MyParser::parse(Rule::expression, &s).unwrap_or_else(|e| panic!("{}", e));
+    pub fn num_tokens(&self) -> i32 {
+        match &self.contents {
+            Contents::Token(_) => 1,
+            Contents::Children(children) => children.iter().map(|x| x.num_tokens()).sum(),
+        }
+    }
 
-    //     // Because ident_list is silent, the iterator will contain idents
-    //     for pair in pairs {
-    //         // A pair is a combination of the rule which matched and a span of input
-    //         println!("Rule:    {:?}", pair.as_rule());
-    //         println!("Span:    {:?}", pair.as_span());
-    //         println!("Text:    {}", pair.as_str());
-
-    //         // A pair can be converted to an iterator of the tokens which make it up:
-    //         for inner_pair in pair.into_inner() {
-    //             match inner_pair.as_rule() {
-    //                 rule => println!("Debug: {:#?}", rule),
-    //             };
-    //         }
-    //     }
-    // }
+    pub fn token_at_index(&self, index: usize) -> Option<Token> {
+        match (&self.contents, index) {
+            (Contents::Token(t), 0) => Some(t.clone()),
+            (Contents::Children(children), _) => {
+                let mut index = index;
+                for c in children {
+                    let len = c.num_tokens() as usize;
+                    if len > index {
+                        return c.token_at_index(index);
+                    } else {
+                        index = index - len;
+                    }
+                }
+                return None;
+            }
+            _ => None,
+        }
+    }
 }
 
 impl fmt::Display for TokenTree {
@@ -186,11 +315,11 @@ impl fmt::Display for TokenTree {
     }
 }
 
-impl fmt::Display for TokenContents {
+impl fmt::Display for Contents {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            TokenContents::Token(s) => write!(f, "{}", s),
-            TokenContents::Children(children) => {
+            Contents::Token(t) => write!(f, "{}", t),
+            Contents::Children(children) => {
                 for child in children {
                     write!(f, "{}", child)?;
                 }
@@ -198,83 +327,6 @@ impl fmt::Display for TokenContents {
             }
         }
     }
-}
-
-// #[derive(Debug, PartialEq)]
-// pub struct Node {
-//     id: u64,
-// }
-
-// #[derive(Debug, PartialEq)]
-// pub enum Delimiter {
-//     Parenthesis,
-//     Brace,
-//     Bracket,
-// }
-
-#[derive(Debug, PartialEq)]
-pub struct Stmt {
-    pub stmtkind: StmtKind,
-    // pub tokens: TokenTree,
-}
-
-#[derive(Debug, PartialEq)]
-pub enum StmtKind {
-    /// just a semicolon
-    EmptyHole,
-    Let(Rc<Pat>, Option<Rc<Type>>, Rc<Expr>, Rc<Expr>),
-    Expr(Expr),
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Expr {
-    pub exprkind: ExprKind,
-    // pub tokens: TokenTree,
-}
-
-#[derive(Debug, PartialEq)]
-pub enum ExprKind {
-    EmptyHole,
-    InvalidText(String),
-    Var(Identifier),
-    Unit,
-    Int(i32),
-    Bool(bool),
-    Str(String),
-    Func(FuncArg, Vec<FuncArg>, Option<Rc<Type>>, Rc<Expr>),
-    If(Rc<Expr>, Rc<Expr>, Rc<Expr>),
-    Block(VecDeque<Rc<Stmt>>),
-    BinOp(Rc<Expr>, Operator, Rc<Expr>),
-    FuncAp(Rc<Expr>, Rc<Expr>, VecDeque<Rc<Expr>>),
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Pat {
-    pub patkind: PatKind,
-    // pub tokens: TokenTree,
-}
-
-#[derive(Debug, PartialEq)]
-pub enum PatKind {
-    EmptyHole,
-    InvalidText(String),
-    Var(Identifier),
-    Unit,
-    Int(i32),
-    Bool(bool),
-    Str(String),
-}
-#[derive(Debug, PartialEq)]
-pub enum Op {
-    Operand(Rc<Expr>),
-    Operator(Operator),
-}
-
-#[derive(Debug, PartialEq)]
-pub enum Operator {
-    EmptyHole,
-    InvalidText(String),
-    Valid(BinOpcode),
 }
 
 #[cfg(test)]
