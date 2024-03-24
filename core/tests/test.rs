663b0a0eeee1f2f25bc2ced840c6ee551ae54e96
Anand Dukkipati <aRdukkipati@gmail.com>
2024-03-23 23:08:01 -0500

    first test

--- core/tests/test.rs
+++ core/tests/test.rs
@@ -1,6 +1,23 @@
-use abra_core;
+use abra_core::*;
 
 #[test]
-fn it_adds_two() {
-  abra_core::abra_hello_world();
+fn basic() {
+    let contents = "2+2".to_owned();
+    let source_file = SourceFile {
+        name: "main.abra".to_owned(),
+        contents,
+    };
+    let prelude = SourceFile {
+        name: "prelude.abra".to_owned(),
+        contents: _PRELUDE.to_string(),
+    };
+    let source_files = vec![prelude, source_file];
+    let runtime = compile::<side_effects::Effect>(source_files);
+    let runtime = runtime.unwrap();
+    let mut interpreter = runtime.toplevel_interpreter();
+    let mut effect_result = None;
+    while !interpreter.is_finished() {
+        interpreter.run(10000, effect_result.take());
+    }
+    assert_eq!(interpreter.get_val().unwrap(), runtime.make_int(4));
 }
