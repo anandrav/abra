b13490663f161b6d63d5fb9deb7a112f906b5c9a
Anand Dukkipati <aRdukkipati@gmail.com>
2024-11-27 17:18:16 -0600

    need to get listutils working, then pivot back to basic Rust native module

--- core/src/addons.rs
+++ core/src/addons.rs
@@ -2,39 +2,39 @@
 
 use crate::vm::Vm;
 
-#[repr(C)]
-pub struct AddonDesc {
-    pub name: &'static str,
-    pub funcs: Array<FuncDesc>,
-}
+// #[repr(C)]
+// pub struct AddonDesc {
+//     pub name: &'static str,
+//     pub funcs: Array<FuncDesc>,
+// }
 
-#[repr(C)]
-pub struct FuncDesc {
-    pub name: &'static str,
-    pub arg_types: Array<Type>,
-    pub ret_type: Type,
-    pub func: fn(&mut Vm),
-}
+// #[repr(C)]
+// pub struct FuncDesc {
+//     pub name: &'static str,
+//     // pub arg_types: Array<Type>,
+//     // pub ret_type: Type,
+//     pub func: fn(&mut Vm),
+// }
 
-#[repr(C)]
-pub enum Type {
-    Unit,
-    Int,
-    Float,
-    Bool,
-    String,
-    Function { args: Array<Type>, ret: *const Type },
-    Tuple(Array<Type>),
-    Nominal(Nominal, Array<Type>),
-}
+// #[repr(C)]
+// pub enum Type {
+//     Unit,
+//     Int,
+//     Float,
+//     Bool,
+//     String,
+//     Function { args: Array<Type>, ret: *const Type },
+//     Tuple(Array<Type>),
+//     Nominal(Nominal, Array<Type>),
+// }
 
-#[repr(C)]
-pub struct Array<T> {
-    pub ptr: *const T,
-    pub len: usize,
-}
+// #[repr(C)]
+// pub struct Array<T> {
+//     pub ptr: *const T,
+//     pub len: usize,
+// }
 
-#[repr(C)]
-pub enum Nominal {
-    Array,
-}
+// #[repr(C)]
+// pub enum Nominal {
+//     Array,
+// }
--- modules/basic/src/lib.rs
+++ modules/basic/src/lib.rs
@@ -5,36 +5,36 @@ use std::ffi::{c_char, CString};
 use abra_core::{addons::*, vm::Vm};
 
 #[no_mangle]
-pub extern "C" fn addon_description() -> *const AddonDesc {
+pub extern "C" fn addon_description() -> () {
     println!("helloooo");
 
     const ADD_NAME: &str = "add";
     const ADDON_NAME: &str = "basic";
 
-    let desc: AddonDesc = AddonDesc {
-        name: ADDON_NAME,
-        funcs: Array {
-            ptr: &[FuncDesc {
-                name: ADD_NAME,
-                arg_types: Array {
-                    ptr: &[Type::Int, Type::Int] as *const Type,
-                    len: 2,
-                },
-                ret_type: Type::Int,
-                func: |vm: &mut Vm| {
-                    let n1 = vm.top().get_int();
-                    vm.pop();
-                    let n2 = vm.top().get_int();
-                    vm.pop();
+    // let desc: AddonDesc = AddonDesc {
+    //     name: ADDON_NAME,
+    //     funcs: Array {
+    //         ptr: &[FuncDesc {
+    //             name: ADD_NAME,
+    //             // arg_types: Array {
+    //             //     ptr: &[Type::Int, Type::Int] as *const Type,
+    //             //     len: 2,
+    //             // },
+    //             // ret_type: Type::Int,
+    //             func: |vm: &mut Vm| {
+    //                 let n1 = vm.top().get_int();
+    //                 vm.pop();
+    //                 let n2 = vm.top().get_int();
+    //                 vm.pop();
 
-                    let ret = add(n1, n2);
-                    vm.push_int(ret);
-                },
-            }] as *const FuncDesc,
-            len: 1,
-        },
-    };
-    &desc
+    //                 let ret = add(n1, n2);
+    //                 vm.push_int(ret);
+    //             },
+    //         }] as *const FuncDesc,
+    //         len: 1,
+    //     },
+    // };
+    // &desc
 }
 
 fn add(n1: i64, n2: i64) -> i64 {
