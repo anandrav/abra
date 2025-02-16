b7c3bda9160ef7f28e759ddc6094450e284e1e4b
Anand Dukkipati <aRdukkipati@gmail.com>
2025-02-16 17:18:45 -0600

    still moving stuff around and experimenting

--- modules/os/src/lib.rs
+++ modules/os/src/lib.rs
@@ -1,58 +1,60 @@
 use abra_core::addons::*;
 use abra_core::vm::Vm;
-use std::fs;
 
-// submodules
-mod exec;
+mod os {
+    use abra_core::addons::*;
+    use abra_core::vm::Vm;
 
-#[export_name = "abra_ffi$os$fread"]
-pub unsafe extern "C" fn fread(vm: *mut Vm) {
-    // define the implementation of "os.abra" in this file
-    #[path = "../os.rs"]
-    mod os;
+    mod _impl;
 
-    // TODO: make a macro for this called get_string!
-    let string_view = abra_vm_view_string(vm);
-    let path = string_view.to_owned();
-    abra_vm_pop(vm);
+    #[export_name = "abra_ffi$os$fread"]
+    pub unsafe extern "C" fn fread(vm: *mut Vm) {
+        // TODO: make a macro for this called get_string!
+        let string_view = abra_vm_view_string(vm);
+        let path = string_view.to_owned();
+        abra_vm_pop(vm);
 
-    let content = os::fread(path);
+        let content = _impl::fread(path);
 
-    let string_view = StringView::from_string(&content);
-    abra_vm_push_string(vm, string_view);
-}
+        let string_view = StringView::from_string(&content);
+        abra_vm_push_string(vm, string_view);
+    }
 
-#[export_name = "abra_ffi$os$fwrite"]
-pub unsafe extern "C" fn fwrite(vm: *mut Vm) {
-    // define the implementation of "os.abra" in this file
-    #[path = "../os.rs"]
-    mod os;
+    #[export_name = "abra_ffi$os$fwrite"]
+    pub unsafe extern "C" fn fwrite(vm: *mut Vm) {
+        let string_view = abra_vm_view_string(vm);
+        let content = string_view.to_owned();
+        abra_vm_pop(vm);
 
-    let string_view = abra_vm_view_string(vm);
-    let content = string_view.to_owned();
-    abra_vm_pop(vm);
+        // TODO: make a macro for this called get_string!
+        let string_view = abra_vm_view_string(vm);
+        let path = string_view.to_owned();
+        abra_vm_pop(vm);
 
-    // TODO: make a macro for this called get_string!
-    let string_view = abra_vm_view_string(vm);
-    let path = string_view.to_owned();
-    abra_vm_pop(vm);
+        _impl::fwrite(path, content);
 
-    os::fwrite(path, content);
+        abra_vm_push_nil(vm);
+    }
 
-    abra_vm_push_nil(vm);
-}
+    mod exec {
+        use abra_core::addons::*;
+        use abra_core::vm::Vm;
 
-#[export_name = "abra_ffi$os$exec$command"]
-pub unsafe extern "C" fn command(vm: *mut Vm) {
-    // define the implementation of "os.abra" in this file
-    // #[path = "exec.rs"] // TODO path necessary here?
-    // mod exec;
+        mod _impl;
 
-    let string_view = abra_vm_view_string(vm);
-    let content = string_view.to_owned();
-    abra_vm_pop(vm);
+        #[export_name = "abra_ffi$os$exec$command"]
+        pub unsafe extern "C" fn command(vm: *mut Vm) {
+            // define the implementation of "os.abra" in this file
+            // #[path = "exec.rs"] // TODO path necessary here?
+            // mod exec;
 
-    let ret = exec::command(content);
+            let string_view = abra_vm_view_string(vm);
+            let content = string_view.to_owned();
+            abra_vm_pop(vm);
 
-    abra_vm_push_int(vm, ret);
+            let ret = _impl::command(content);
+
+            abra_vm_push_int(vm, ret);
+        }
+    }
 }
