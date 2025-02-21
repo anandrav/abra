ec99ca5bc37e157b3a84a250f11b38eb2c6faebb
Anand Dukkipati <aRdukkipati@gmail.com>
2025-02-21 02:57:36 -0600

    update time to 2024 edition

--- modules/time/rust_project/src/lib.rs
+++ modules/time/rust_project/src/lib.rs
@@ -5,16 +5,16 @@ pub mod ffi {
     pub mod time {
         use crate::time;
         use abra_core::addons::*;
-        #[export_name = "abra_ffi$time$get_time"]
-        pub unsafe extern "C" fn get_time(vm: *mut Vm) {
+        #[unsafe(export_name = "abra_ffi$time$get_time")]
+        pub unsafe extern "C" fn get_time(vm: *mut Vm) { unsafe {
             let ret: f64 = time::get_time();
             ret.to_vm(vm);
-        }
-        #[export_name = "abra_ffi$time$sleep"]
-        pub unsafe extern "C" fn sleep(vm: *mut Vm) {
+        }}
+        #[unsafe(export_name = "abra_ffi$time$sleep")]
+        pub unsafe extern "C" fn sleep(vm: *mut Vm) { unsafe {
             let seconds = f64::from_vm(vm);
             let ret: () = time::sleep(seconds);
             ret.to_vm(vm);
-        }
+        }}
     }
 }
