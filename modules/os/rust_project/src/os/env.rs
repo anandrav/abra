61c46198b7f5d7dcdd80e4cfd3c919fab128aa7f
Anand Dukkipati <aRdukkipati@gmail.com>
2025-03-28 18:26:31 -0500

    added MPL2 notice to files in modules/

--- modules/os/rust_project/src/os/env.rs
+++ modules/os/rust_project/src/os/env.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 pub fn get_var(key: String) -> String {
     println!("fn get_var({key})");
     std::env::var(key).unwrap_or("".to_string())
