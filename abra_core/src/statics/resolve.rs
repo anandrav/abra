c3bf2428b64f97697c817fa2ccefcd808ba71741
Anand Dukkipati <aRdukkipati@gmail.com>
2025-12-31 13:08:00 -0500

    remove unnecessary code

--- abra_core/src/statics/resolve.rs
+++ abra_core/src/statics/resolve.rs
@@ -822,11 +822,6 @@ fn resolve_names_expr(ctx: &mut StaticsContext, symbol_table: &SymbolTable, expr
                     resolve_names_expr(ctx, symbol_table, arg);
                 }
             }
-            ExprKind::MemberAccessLeadingDot(_) => {
-                for arg in args {
-                    resolve_names_expr(ctx, symbol_table, arg);
-                }
-            }
             _ => {
                 resolve_names_expr(ctx, symbol_table, func);
                 for arg in args {
