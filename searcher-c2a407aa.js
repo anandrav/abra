724f62858f3d55041287599d73b8ea1e2a1970b8
anandrav <anandrav@users.noreply.github.com>
2026-04-09 21:24:16 +0000

    deploy: 2f2361b1e211443415633d249b7437bb45615801

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-77fdeb5f.js',
+                path_to_root + 'searchindex-ca073505.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
